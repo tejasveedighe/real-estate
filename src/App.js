6c9c74d0f4510cace6f386bf452298b54b054639
Tejasvee Dighe <tejasvee.d@cisinlabs.com>
2024-02-20 16:22:35 +0530

    Footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
