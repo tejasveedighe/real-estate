d80f0f0dc413b43f2b86b34b5c485687fd852f27
Tejasvee Dighe <tejasveedighe@gmail.com>
2024-02-22 11:29:11 +0530

    Property card image size and overflow fix

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Card, ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import styles from "./PropertyCard.module.css";
 
 function PropertyCard({ property, index }) {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img
         variant="top"
+        className={styles.image}
         src={`${process.env.PUBLIC_URL}/house${index}.jpg`}
       />
       <Card.Body>
-        <Card.Title>{property?.propertyTitle}</Card.Title>
-        <Card.Text>{property?.description}</Card.Text>
+        <Card.Title className={styles.title}>
+          {property?.propertyTitle}
+        </Card.Title>
       </Card.Body>
       <ListGroup className="list-group-flush">
         <ListGroup.Item>Location: {property?.location}</ListGroup.Item>
