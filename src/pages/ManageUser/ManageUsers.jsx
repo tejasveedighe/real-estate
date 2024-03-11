b5cdbd667021b5a364bd2e43e61779cd3876b161
Tejasvee Dighe <tejasveedighe@gmail.com>
2024-03-11 18:41:13 +0530

    Update the users table to have filters

--- src/pages/ManageUser/ManageUsers.jsx
+++ src/pages/ManageUser/ManageUsers.jsx
@@ -1,18 +1,27 @@
 import Cookies from "js-cookie";
 import React, { useCallback, useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import AddNewUserModal from "../../components/AddNewUserModal/AddNewUserModal";
-import UserProfileCard from "../../components/UserProfileCard/UserProfileCard";
-import { deleteUserById, getAllUsers } from "../../redux/slices/userSlice";
-import styles from "./ManageUser.module.css";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+import {
+  deleteUserById,
+  getAllUsers,
+  setUser,
+} from "../../redux/slices/userSlice";
+import { toast } from "react-toastify";
+import { UserTable } from "./UserTable";
+import classNames from "classnames";
+import styles from "./ManageUser.module.css";
+import { UserDataFeatures } from "./UserDataFeatures";
 
 function ManageUser() {
   const dispatch = useDispatch();
 
   const { users, loading, status } = useSelector((store) => store.user);
   const currentUserId = parseInt(Cookies.get("userId"));
+
+  const [userType, setUserType] = useState("");
 
   const [filteredUser, setFilteredUsers] = useState([]);
   useEffect(() => {
@@ -27,6 +36,25 @@ function ManageUser() {
     }
   }, [users, currentUserId, loading]);
 
+  const [count, setCount] = useState({
+    buyers: 0,
+    sellers: 0,
+  });
+  useEffect(() => {
+    setCount(() => {
+      let buyers = 0;
+      let sellers = 0;
+      for (const user of users) {
+        if (user.userType === "Seller") sellers++;
+        else if (user.userType === "Buyer") buyers++;
+      }
+      return {
+        buyers,
+        sellers,
+      };
+    });
+  }, [users]);
+
   const [openAddNewUserModal, setOpenAddNewUserModal] = useState(false);
   const handleAddNewUser = useCallback(() => {
     setOpenAddNewUserModal((prev) => !prev);
@@ -34,13 +62,25 @@ function ManageUser() {
 
   const handleDeleteUser = useCallback(
     (userId) => {
-      dispatch(deleteUserById(userId)).then((res) => {
-        alert("User was deleted");
-        dispatch(getAllUsers());
-      });
+      dispatch(deleteUserById(userId))
+        .then((res) => {
+          toast.success("User was deleted");
+          dispatch(getAllUsers());
+        })
+        .catch((err) => toast.error(err.message));
     },
     [dispatch]
   );
+
+  useEffect(() => {
+    let filtered;
+    if (userType === "") {
+      filtered = users;
+    } else {
+      filtered = users.filter((user) => user.userType === userType);
+    }
+    setFilteredUsers(filtered);
+  }, [userType, users]);
 
   if (loading) {
     return (
@@ -58,21 +98,42 @@ function ManageUser() {
       </main>
     );
   }
+
+  const handleSearchChange = (e) => {
+    const searchQuery = e.target.value;
+  };
+
   return (
-    <main className="d-flex flex-column align-items-center justify-content-center">
-      <div className="d-flex align-items-center">
+    <main className={classNames(styles.parent, "container")}>
+      <div className="d-flex align-items-center justify-content-center">
         <h1 className="text-center p-5 d-inline-block">Manage Users</h1>
         <Button onClick={handleAddNewUser}>Add New User</Button>
       </div>
-      <div className={styles.usersGrid}>
-        {filteredUser.map((user) => (
-          <UserProfileCard
-            key={user.userId}
-            user={user}
-            deleteUser={handleDeleteUser}
+      <UserDataFeatures users={users} count={count} />
+      <div className={classNames(styles.filterContainer, "rounded")}>
+        {/* Search Input Continaer */}
+        <div>
+          <p>User Type</p>
+          <Form.Select onChange={(e) => setUserType(e.target.value)}>
+            <option value="" defaultChecked>All Types</option>
+            <option value="Seller">Seller</option>
+            <option value="Buyer">Buyer</option>
+          </Form.Select>
+        </div>
+
+        {/* Search Input Continaer */}
+        <div>
+          <p>Search</p>
+          <Form.Control
+            placeholder="Enter query"
+            onChange={handleSearchChange}
           />
-        ))}
+        </div>
       </div>
+      <UserTable
+        filteredUser={filteredUser}
+        handleDeleteUser={handleDeleteUser}
+      />
       <AddNewUserModal onHide={handleAddNewUser} show={openAddNewUserModal} />
     </main>
   );
